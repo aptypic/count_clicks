dbba16cb9daee0f37178f30f1dc0fc85836b3e1b
asivolap <asivolap@openwaygroup.com>
2021-09-09 16:35:09 +0300

    added the exceptions in main()

--- count_clicks.py
+++ count_clicks.py
@@ -1,54 +1,48 @@
 import os
+
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 import requests
 
 load_dotenv()
-short_link = "https://api-ssl.bitly.com/v4/shorten"
-count_link = "https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary"
-token = os.getenv("TOKEN")
 
 
-def count_clicks(token, count_link, user_input):
-    url = count_link.format(user_input)
+def count_clicks(count_link, headers, a):
+    b = urlparse(a)
+    b = b._replace(scheme='')
+    a = b.geturl()
+    url = count_link.format(a)
     api_token = {
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {headers}",
     }
-    try:
-        response = requests.get(url, headers=api_token)
-        response.raise_for_status()
-        return response.json().get("total_clicks")
-    except requests.exceptions.HTTPError:
-        print("Неверный формат (пример: bit.ly/3vds2)")
+    response = requests.get(url, headers=api_token)
+    response.raise_for_status()
+    return response.json().get("total_clicks")
 
 
-def shorten_link(token, short_link, url):
+def shorten_link(token_bitly, short_link, url):
     api_token = {
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {token_bitly}",
     }
     manual_url = {
         "long_url": url,
     }
-    try:
-        response = requests.post(short_link, headers=api_token, json=manual_url)
-        response.raise_for_status()
-        return response.json().get("link")
-    except requests.exceptions.HTTPError:
-        print("Неверный формат (пример: https://gmail.com)")
-
-
-def recognize_request():
-    user_input = (input())
-    parsing_input = urlparse(user_input)
-    if parsing_input.scheme == "https" or parsing_input.scheme == "http":
-        print(shorten_link(token, short_link, user_input))
-    else:
-        print("Общее количество кликов =", count_clicks(token, count_link, user_input))
+    response = requests.post(short_link, headers=api_token, json=manual_url)
+    response.raise_for_status()
+    return response.json().get("link")
 
 
 def main():
+    token_bitly = os.getenv("TOKEN")
+    count_link = os.getenv("count_link")
+    short_link = os.getenv("short_link")
     print("Пожалуйста, напишите url")
-    recognize_request()
+    a = input()
+    try:
+        print("Общее количество кликов =", count_clicks(count_link,
+                                                        token_bitly, a))
+    except requests.exceptions.HTTPError:
+        print(shorten_link(token_bitly, short_link, a))
 
 
 if __name__ == "__main__":
